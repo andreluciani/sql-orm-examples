fb3889e08cc9cbdb1327a2c6161571c21a4df95e
André Luciani <andrelucianidasilva@gmail.com>
2023-06-29 00:25:41 +0000

    updated gitpod configs

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get update
+sudo rm -f google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get -f install -y
