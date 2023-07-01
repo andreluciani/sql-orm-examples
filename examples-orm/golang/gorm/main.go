431c18ef89f295dbbdab446b8c53449e191e754b
André Luciani <andrelucianidasilva@gmail.com>
2023-07-01 20:23:58 +0000

    added GORM to endpoint

--- examples-orm/golang/gorm/main.go
+++ examples-orm/golang/gorm/main.go
@@ -3,14 +3,48 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+var db *gorm.DB
+
+type Quotes struct {
+	ID    uint `gorm:"primaryKey"`
+	Quote string
+}
+
+func getRandomQuote() (string, error) {
+	var quote string
+	err := db.Select("quote").First(&quote).Error
+	if err != nil {
+		return "", err
+	}
+
+	return quote, nil
+}
+
 func quoteHandler(w http.ResponseWriter, r *http.Request) {
+	quote, err := getRandomQuote()
+	if err != nil {
+		log.Println("Failed to retrieve quote:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("Nothing is impossible.\n"))
+	w.Write([]byte(quote))
 }
 
 func main() {
+	dsn := "host=localhost port=5432 sslmode=disable"
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect to database")
+	}
+
 	http.HandleFunc("/quote", quoteHandler)
 
 	log.Println("Listening on port 8080")
--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ sudo rm -f google-chrome-stable_current_amd64.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get -f install -y
+
+# unset PGHOSTADDR
